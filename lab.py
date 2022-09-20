ef2b896a50967b4c2074250465d20bb6a6b1bf26
Daria Zharova <103378546+Zharovaa@users.noreply.github.com>
2022-09-20 13:10:19 +0300

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,8 @@
+import math
+R = int(input("Radius equals to:"))
+h = int(input("Height equals to:"))
+PI = math.pi
+V =  PI * R ** 2 * h
+S = 2 * PI * R * h
+print("Volume is:", V)
+print("The area is:", S)
